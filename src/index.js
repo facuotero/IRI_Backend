e13eaf5c191d92d8eaa783e144abde2f13196509
FOteroRimini <facurimini@gmail.com>
2024-02-01 01:17:57 -0300

    trying

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const airtableController = require('./routes/airtableRouter');
